ee3075fe9d791dde71e036e3aaebbe1870a7dee3
Lisi <lisiw@clemson.edu>
2020-07-16 22:09:45 -0400

    clean state

--- website/src/components/TodoContainer.js
+++ website/src/components/TodoContainer.js
@@ -8,21 +8,6 @@ import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
  todos: [
-   {
-     id: 1,
-     title: "Setup development environment",
-     completed: true
-   },
-   {
-     id: 2,
-     title: "Develop website and add content",
-     completed: false
-   },
-   {
-     id: 3,
-     title: "Deploy to live server",
-     completed: false
-   }
  ]
 };
   handleChange = id => {
